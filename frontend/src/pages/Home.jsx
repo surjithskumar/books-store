59a15683482861c962d5acb2e350e22c76138a01
surjith <101186141+surjithskumar@users.noreply.github.com>
2024-12-20 21:49:09 +0530

    backend link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://books-store-backend-8yw9.onrender.com/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
